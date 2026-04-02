3d57f40f20f011cac6fdb74d1f77d641a126ba79
Nina Adora <nirinakaes@gmail.com>
2026-04-02 21:15:08 +0300

    Fix syntax error in updateProductName function

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -16,7 +16,8 @@ console.log(addProduct("Tablet"))
 console.log(products)
 // a function called updateProductName to change the name of a product. This function should take the product's position in the array and the new name as arguments.
 
-function updateProductName (index,newName){products.with(index,newName)return products)
+function updateProductName (index,newName){products.with(index,newName);
+  return products;
 }
 console.log(updateProductName(1,"Motherboard"))
 
