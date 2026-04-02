ae54e61f36de4a5dd3e38d4eb987669436562641
Nina Adora <nirinakaes@gmail.com>
2026-04-02 12:28:22 +0300

    create a function that shows the 1st product

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,29 @@
-// Write your code here
+// //create an array called products
+const products= ["Laptop","Phone","Headphones","Monitor"]
+
+// //a function called logFirstProduct to console log the details of the first product in the array.
+
+// function logFirstProduct(productOne){ {return productOne[0]}
+// }
+// console.log(logFirstProduct(products))
+
+// a function called addProduct to add a new product to the array. This function should take the product name as an argument.
+
+function addProduct(productName){ return
+
+}
+
+// a function called updateProductName to change the name of a product. This function should take the product's position in the array and the new name as arguments.
+
+function updateProductName(){
 
 
+}
+//Write a function called removeLastProduct to remove the last product from the array.
+
+function removeLastProduct(){
+
+}
 
 // Export the necessary parts for testing
 module.exports = {
