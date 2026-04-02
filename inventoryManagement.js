b1c6668e84136e36b0581803436933fdd88d5e28
Nina Adora <nirinakaes@gmail.com>
2026-04-02 20:57:05 +0300

    Refactor inventory management functions and add new file for product operations

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,29 +1,30 @@
 // //create an array called products
 const products= ["Laptop","Phone","Headphones","Monitor"]
-
+console.log (products)
 // //a function called logFirstProduct to console log the details of the first product in the array.
 
-// function logFirstProduct(productOne){ {return productOne[0]}
-// }
-// console.log(logFirstProduct(products))
+function logFirstProduct (productOne){ {return productOne[0]}
+}
+console.log(logFirstProduct(products))
 
 // a function called addProduct to add a new product to the array. This function should take the product name as an argument.
 
-function addProduct(productName){ return
-
+function addProduct (productName){ return products.push(productName)
 }
-
+console.log(addProduct("Tablet"))
+console.log(products)
 // a function called updateProductName to change the name of a product. This function should take the product's position in the array and the new name as arguments.
 
-function updateProductName(){
-
-
+function updateProductName (index,newName){ return products.with(index,newName)
 }
+console.log(updateProductName([1],"Motherboard"))
+
 //Write a function called removeLastProduct to remove the last product from the array.
 
-function removeLastProduct(){
-
+function removeLastProduct(){ return products.pop()
 }
+console.log(removeLastProduct(products))
+console.log(products)
 
 // Export the necessary parts for testing
 module.exports = {
